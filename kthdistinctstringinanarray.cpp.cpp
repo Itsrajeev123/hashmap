a4831ffe230dfb46bffef1abdc3ec656bcd13e3b
Rajeev mishra <113230206+Itsrajeev123@users.noreply.github.com>
2023-02-13 14:31:13 +0530

    Create kthdistinctstringinanarray.cpp.cpp

---
+++ kthdistinctstringinanarray.cpp.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int>map;
+        for(string i:arr){
+             map[i]++;
+        }
+        string s="";
+        for(auto i:arr){
+            if(map[i]==1)
+               k--;
+            if(k==0)
+               return i;
+        }
+        return s;
+    }
+};
+Footer
+© 2023 GitHub, Inc.
